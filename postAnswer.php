6c7e9fa770cd987c816837ee2eeb0ded1ccf8478
SHJing <lexons95@hotmail.com>
2017-11-13 01:01:58 +0800

    Add files via upload

--- postAnswer.php
+++ postAnswer.php
@@ -18,26 +18,11 @@
 	$description =$_POST['description'];
 	$date1 = new DateTime();
 	$date1 = $date1->format("Y-m-d");
-
+	
+	
 	if (isset($_POST['post'])) {	
-		
-		$file = rand(1000,100000)."-".$_FILES['file']['name'];
-		$file_loc = $_FILES['file']['tmp_name'];
-		$file_size = $_FILES['file']['size'];
-		$file_type = $_FILES['file']['type'];
-		$folder="imageUploaded/";
-		 
-		// new file size in KB
-		$new_size = $file_size/1024;  
-		// new file size in KB
-		 
-		// make file name in lower case
-		$new_file_name = strtolower($file);
-		// make file name in lower case 
-		$final_file=str_replace(' ','-',$new_file_name);
-				 
-		if(move_uploaded_file($file_loc,$folder.$final_file)){
-
+		if ($_FILES["file"]["error"] == 4) {
+			$final_file = "";
 			if (!empty($title)) {
 				$stmt = "INSERT INTO posts (postTitle, itemType, itemModal, problemType, repairMethodRequested, problemDescription, datePublished, image, userId) VALUES (:title, :type, :modal, :problem, :method, :description, :date, :image, :id)";
 				$p = $MySQLi_CON -> prepare($stmt);
@@ -71,12 +56,64 @@
 				exit();
 			} 
 		}
-		else{
-			echo '<script language = "javascript">';
-			echo 'alert("Error while uploading file")';
-			echo '</script>';
-			header("Location: " . $_SERVER['HTTP_REFERER']);
+		else {
+			$file = rand(1000,100000)."-".$_FILES['file']['name'];
+			$file_loc = $_FILES['file']['tmp_name'];
+			$file_size = $_FILES['file']['size'];
+			$file_type = $_FILES['file']['type'];
+			$folder="imageUploaded/";
+			
+			// new file size in KB
+			$new_size = $file_size/1024;  
+			// new file size in KB
+			
+			// make file name in lower case
+			$new_file_name = strtolower($file);
+			// make file name in lower case 
+			$final_file=str_replace(' ','-',$new_file_name);
+			if(move_uploaded_file($file_loc,$folder.$final_file)){
+
+				if (!empty($title)) {
+					$stmt = "INSERT INTO posts (postTitle, itemType, itemModal, problemType, repairMethodRequested, problemDescription, datePublished, image, userId) VALUES (:title, :type, :modal, :problem, :method, :description, :date, :image, :id)";
+					$p = $MySQLi_CON -> prepare($stmt);
+
+					$results = $p -> execute(array(
+						":title" => $title,
+						":type" => $type,
+						":modal" => $modal,
+						":problem" => $problem,
+						":method" => $method,
+						":date" => $date1,
+						":id" => $id,
+						":image" => $final_file,
+						":description" => $description
+					));
+					echo '<script language = "javascript">';
+					echo 'alert("Post successfully")';
+					echo '</script>';
+					echo  "<script> window.location.assign('solvit2.php'); </script>";
+					if(!$results){
+						echo '<script language = "javascript">';
+						echo 'alert("Fail to post")';
+						echo '</script>';
+					}
+				}
+				else{
+					echo '<script language = "javascript">';
+					echo 'alert("Please fill all the field.")';
+					echo '</script>';
+					header("Location: " . $_SERVER['HTTP_REFERER']);
+					exit();
+				} 
+			}
+			else{
+				echo '<script language = "javascript">';
+				echo 'alert("Error while uploading file")';
+				echo '</script>';
+				header("Location: " . $_SERVER['HTTP_REFERER']);
+			}
 		}
+				
 	}
 
 
