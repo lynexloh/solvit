d6b0b86d7b23a6653fd3ecc68e88221e31e63505
SHJing <lexons95@hotmail.com>
2017-11-13 00:47:02 +0800

    Add files via upload

---
+++ paypal_cancel.php
@@ -0,0 +1,6 @@
+<?php
+		echo '<script language = "javascript">';
+		echo 'alert("Your PayPal transaction has been canceled.")';
+		echo '</script>';
+		echo  "<script> window.location.assign('order.php'); </script>";
+?>
